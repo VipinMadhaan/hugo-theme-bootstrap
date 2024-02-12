a45df10a6e504cbe6588e49a8ee3bf3d6e2d9425
Vipin Kumar Madhaan <Vipin.Madhaan@gmail.com>
2024-02-12 13:45:37 +0530

    Fixed comment system

--- assets/js/giscus/index.ts
+++ assets/js/giscus/index.ts
@@ -1,43 +1,41 @@
-import { giscus as giscusParams } from "@params";
-import LocalStorage from "js/local-storage";
+import * as params from "@params";
+import { default as LocalStorage } from "js/local-storage";
+import ModeToggle from "js/mode";
 
 class Giscus {
-  constructor() {
-    this.theme = giscusParams.theme;
-  }
-
   run() {
-    if (!this.theme) {
-      document.addEventListener("hbs:mode", this.handleModeChange);
-      setTimeout(this.handleModeChange, 2000);
+    const theme = params.giscus.theme;
+    if (!theme) {
+      document.addEventListener("hbs:mode", (e: CustomEvent) => {
+        this.handleThemeChange(e.detail.mode);
+      });
+      setTimeout(() => {
+        this.handleThemeChange(LocalStorage.getItem("mode"));
+      }, 2000);
     }
   }
 
-  handleModeChange = (e) => {
-    const mode = e && e.detail && e.detail.mode;
-    const theme = this.getTheme(mode || LocalStorage.getItem("mode"));
-    this.rerender(theme);
-  };
-
-  getTheme(mode) {
+  handleThemeChange(mode) {
     if (mode === "auto") {
       mode = ModeToggle.getPreferredMode();
     }
-    return mode === "dark" ? "dark" : "light";
+    const theme = mode === "dark" ? "dark" : "light";
+    this.updateIframeTheme(theme);
   }
 
-  rerender(theme) {
+  updateIframeTheme(theme) {
     const iframe = document.querySelector<HTMLIFrameElement>(
       "iframe.giscus-frame"
     );
     if (!iframe) {
       return;
     }
+    const themeUrl = "https://giscus.app/themes/" + theme + ".css";
     iframe.contentWindow.postMessage(
       {
         giscus: {
           setConfig: {
-            theme: "https://giscus.app/themes/" + theme + ".css",
+            theme: themeUrl,
           },
         },
       },
@@ -46,9 +44,6 @@ class Giscus {
   }
 }
 
-function initializeGiscus() {
-  const giscus = new Giscus();
-  giscus.run();
-}
-
-document.addEventListener("DOMContentLoaded", initializeGiscus);
+document.addEventListener("DOMContentLoaded", () => {
+  new Giscus().run();
+});
